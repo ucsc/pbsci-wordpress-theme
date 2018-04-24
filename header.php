9f4eebe5efe9d9bad94be12aafe3ce451af66867
Jason Chafin <jchafin@ucsc.edu>
2018-04-24 14:55:41 -0700

    When the kids go Go Go Crazy tonight.

--- header.php
+++ header.php
@@ -23,7 +23,7 @@
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'ucsc-underscore' ); ?></a>
-
+	<?php get_template_part('template-parts/header','top');?>
 	<header id="masthead" class="site-header main-header">
 		<div class="site-branding row names">
 			<?php
