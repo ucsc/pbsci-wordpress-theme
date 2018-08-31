e47697d75d002aad85997dd3f9e291b21a05e7bc
Jason Chafin <jchafin@ucsc.edu>
2018-08-31 14:57:01 -0700

    removing gallery-block div from sidebar-home-top-right

--- template-parts/homepanel-top.php
+++ template-parts/homepanel-top.php
@@ -5,23 +5,23 @@
 echo '<div class="row three separate dark">';
 
 echo '<div class="wrap">';
-     if ( is_active_sidebar( 'sidebar-home-top-left' ) ) : 
-     dynamic_sidebar( 'sidebar-home-top-left' ); 
+     if ( is_active_sidebar( 'sidebar-home-top-left' ) ) :
+     dynamic_sidebar( 'sidebar-home-top-left' );
  endif;
 echo '</div>';
 
 echo '<div class="wrap">';
-     if ( is_active_sidebar( 'sidebar-home-top-center' ) ) : 
-     dynamic_sidebar( 'sidebar-home-top-center' ); 
+     if ( is_active_sidebar( 'sidebar-home-top-center' ) ) :
+     dynamic_sidebar( 'sidebar-home-top-center' );
  endif;
 echo '</div>';
 
 echo '<div class="wrap last">';
-echo '<div class="block gallery-block" id="view">';
-     if ( is_active_sidebar( 'sidebar-home-top-right' ) ) : 
-     dynamic_sidebar( 'sidebar-home-top-right' ); 
+// echo '<div class="block gallery-block" id="view">';
+     if ( is_active_sidebar( 'sidebar-home-top-right' ) ) :
+     dynamic_sidebar( 'sidebar-home-top-right' );
  endif;
-echo '</div>';
+// echo '</div>';
 
 echo '</div>';
 echo '</div>';
