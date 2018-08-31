75087e97c55d851194f68272b5ecef0fcd8de09f
Jason Chafin <jchafin@ucsc.edu>
2018-08-31 13:39:22 -0700

    working on widget-fix.js

--- js/localist-widget-fix.js
+++ js/localist-widget-fix.js
@@ -1,18 +1,11 @@
-(function() {
-    var date = document.querySelector('.lw_event_item_dates').innerHTML;
-    var split = date.split(" ");
-    var days = ["01", "02", "02", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"];
+    var date = document.getElementsByClassName('lw_event_item_dates');
 
-    for (var i = 0; i < split.length; i++) {
-        for (var j = 0; j < days.length; j++) {
-            if (split[i] == days[j]) {
-                split[i] = "<span>" + split[i] + "</span>";
-            }
-        }
+    var days = ["1", "2", "2", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"];
+    for (var i=0; i<date.length; i++) {
+       var text = date[i].innerHTML.split(" ");
+
+       text[2]= "<br><span>" + text[2] + "</span>";
+       console.log(text[2]);
+       var output = text.join('');
+       date[i].innerHTML = output;
     }
-
-    var newDate = split.join(' ');
-
-    document.querySelector('.lw_event_item_dates').innerHTML = newDate;
-    //console.log(split);
-})();
