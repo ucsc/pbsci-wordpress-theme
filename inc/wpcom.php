2cf304e8e9ae1de4b718fc2cd57bc76ce3a781c5
Jason Chafin <jchafin@ucsc.edu>
2019-01-18 16:10:17 -0800

    initial commit of new blank theme

---
+++ inc/wpcom.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * WordPress.com-specific functions and definitions
+ *
+ * This file is centrally included from `wp-content/mu-plugins/wpcom-theme-compat.php`.
+ *
+ * @package UCSC_PBSci
+ */
+
+/**
+ * Adds support for wp.com-specific theme functions.
+ *
+ * @global array $themecolors
+ */
+function ucsc_pbsci_wpcom_setup() {
+	global $themecolors;
+
+	// Set theme colors for third party services.
+	if ( ! isset( $themecolors ) ) {
+		// Whitelist wpcom specific variable intended to be overruled.
+		// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
+		$themecolors = array(
+			'bg'     => '',
+			'border' => '',
+			'text'   => '',
+			'link'   => '',
+			'url'    => '',
+		);
+	}
+}
+add_action( 'after_setup_theme', 'ucsc_pbsci_wpcom_setup' );
