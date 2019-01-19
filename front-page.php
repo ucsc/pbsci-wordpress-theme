e11258300e59e220a65e453a0222f2a88c5d3834
Jason Chafin <jchafin@ucsc.edu>
2019-01-18 16:48:28 -0800

    converting underscores to pbsci

--- front-page.php
+++ front-page.php
@@ -15,26 +15,14 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="no-sidebar content-area">
 		<main id="main" class="site-main">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
+		Hello World
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
+
 get_footer();
