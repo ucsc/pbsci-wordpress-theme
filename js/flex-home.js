eda0b601bfcfdefccd46c82adcaf79aee26e1ee1
Jason Chafin <jchafin@ucsc.edu>
2019-02-08 16:32:29 -0800

    working on home hero

--- js/flex-home.js
+++ js/flex-home.js
@@ -1,10 +1,47 @@
-$(window).load(function() {
-    $('.flexslider').flexslider({
-      animation: "slide",
-      animationLoop: false,
-      itemWidth: 210,
-      itemMargin: 5,
-      minItems: 3,
-      maxItems: 3
+// $(window).load(function() {
+//     $('.flexslider').flexslider({
+//       animation: "slide",
+//       animationLoop: false,
+//       itemWidth: 210,
+//       itemMargin: 50,
+//       minItems: 1,
+//       maxItems: 3
+//     });
+//   });
+
+  //And then there's this one
+  (function() {
+
+    // store the slider in a local variable
+    var $window = $(window),
+        flexslider = { vars:{} };
+
+    // tiny helper function to add breakpoints
+    function getGridSize() {
+      return (window.innerWidth < 600) ? 1 :
+             (window.innerWidth < 900) ? 2 : 3;
+    }
+
+    $(function() {
+      SyntaxHighlighter.all();
     });
-  });
+
+    $window.load(function() {
+      $('.flexslider').flexslider({
+        animation: "slide",
+        animationLoop: false,
+        itemWidth: 500,
+        itemMargin: 5,
+        minItems: getGridSize(), // use function to pull in initial value
+        maxItems: getGridSize() // use function to pull in initial value
+      });
+    });
+
+    // check grid size on resize event
+    $window.resize(function() {
+      var gridSize = getGridSize();
+
+      flexslider.vars.minItems = gridSize;
+      flexslider.vars.maxItems = gridSize;
+    });
+  }());
--- template-parts/hero-home.php
+++ template-parts/hero-home.php
@@ -6,22 +6,20 @@ $hero = get_field('hero_image', 132);
 // print_r($hero);
 if($hero){
     echo '<div class="hero-home"
-    style="background:url('.$hero.') no-repeat top; background-size: cover;">';}
+    style="background:url('.$hero.') no-repeat top; background-size: cover; background-position-y: -200px">';}
     else {
         echo '<div class="hero-home">';
     }
 ?>
 <div class="wrap flex-column flex-wrap">
 <div class="flexslider carousel">
-    <ul class="slides home-stats">
-        <li>One Stat</li>
-        <li>Two Stat</li>
-        <li>Three Stat</li>
-        <li>Four Stat</li>
-        <li>One Stat</li>
-        <li>Two Stat</li>
-        <li>Three Stat</li>
-        <li>Four Stat</li>
+    <ul class="slides stats-home">
+        <li><div class="stats-container"><div class="stats-rank"><p>#5</p></div><div class="stats-meta"><p class="stats-headline">Global ranking for science reserach influence</p><p class="stats-source">Source: THE 2019</p></div></div></li>
+        <li><div class="stats-container"><div class="stats-rank"><p>#5</p></div><div class="stats-meta"><p class="stats-headline">Global ranking for science reserach influence</p><p class="stats-source">Source: THE 2019</p></div></div></li>
+        <li><div class="stats-container"><div class="stats-rank"><p>#5</p></div><div class="stats-meta"><p class="stats-headline">Global ranking for science reserach influence</p><p class="stats-source">Source: THE 2019</p></div></div></li>
+        <li><div class="stats-container"><div class="stats-rank"><p>#5</p></div><div class="stats-meta"><p class="stats-headline">Global ranking for science reserach influence</p><p class="stats-source">Source: THE 2019</p></div></div></li>
+        <li><div class="stats-container"><div class="stats-rank"><p>#5</p></div><div class="stats-meta"><p class="stats-headline">Global ranking for science reserach influence</p><p class="stats-source">Source: THE 2019</p></div></div></li>
+        <li><div class="stats-container"><div class="stats-rank"><p>#5</p></div><div class="stats-meta"><p class="stats-headline">Global ranking for science reserach influence</p><p class="stats-source">Source: THE 2019</p></div></div></li>
     </ul>
 </div>
 
