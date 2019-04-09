7e3ca63ec1a7f8140e676a06a1270b39a532e68c
Jason Chafin <jchafin@ucsc.edu>
2019-04-09 13:24:46 -0700

    added url links to source on home slider, working on adjusting the background topo.

--- header-home.php
+++ header-home.php
@@ -45,7 +45,7 @@
             if($slideRows){
                 echo '<div class="flex-column flex-wrap"><div class="hero-hidden"></div><div class="panel"><div class="wrap"><div class="flexslider carousel"><ul class="slides stats-home">';
                 foreach ($slideRows as $slideRow){
-                    echo '<li><div class="stats-container"><div class="stats-rank"><p>#'.$slideRow['stat_rank'].'</p></div><div class="stats-meta"><p class="stats-headline">'.$slideRow['stat_heading'].'</p><p class="stats-source">Source: '.$slideRow['stat_source'].'</p></div></div> </li>';
+                    echo '<li><div class="stats-container"><div class="stats-rank"><p>#'.$slideRow['stat_rank'].'</p></div><div class="stats-meta"><p class="stats-headline">'.$slideRow['stat_heading'].'</p><p class="stats-source">Source: <a href="'.$slideRow['source_link'].'">'.$slideRow['stat_source'].'</a></p></div></div> </li>';
                 }
                 echo'</ul></div></div>';
             }
