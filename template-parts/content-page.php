bd6142dcc317400b28f98388d2f52dad46a37a03
Jason Chafin <jchafin@ucsc.edu>
2019-06-07 21:14:10 -0700

    finished first draft of secondary nav

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -20,67 +20,80 @@ $page_blurb = get_field('page_blurb');
         $secondaryMenu = 'menu-2';
     }
     if(is_page('academics')||is_page('research')){
-        $secondaryNav1 = '#da216d';//magenta
-        $secondaryNav2 = '#93c02d';//green
-        $secondaryNav3 = '#007988';//aquamarine
+        $secondaryNavCustomRows = get_field('secondary_navigation_customization');
+        if($secondaryNavCustomRows){
+            $customRowsCount = count ($secondaryNavCustomRows);
+            if(($customRowsCount < 4) && ($customRowsCount > 2)){
+                $customRowOne = $secondaryNavCustomRows[0];
+                $customRowTwo = $secondaryNavCustomRows[1];
+                $customRowThree = $secondaryNavCustomRows[2];
+            }
+            // for($i=0;$i<$customRowsCount;$i++){
+            // }
+        }
+        $secondaryNav1 = $customRowOne['secondary_navigation_label'];
+        $secondaryIcon1 = $customRowOne['secondary_navigation_icon'];
+        $secondaryIconColor1 = $customRowOne['secondary_navigation_icon_color'];
+        $secondaryNav2 = $customRowTwo['secondary_navigation_label'];
+        $secondaryIcon2 = $customRowTwo['secondary_navigation_icon'];
+        $secondaryIconColor2 = $customRowTwo['secondary_navigation_icon_color'];
+        $secondaryNav3 = $customRowThree['secondary_navigation_label'];
+        $secondaryIcon3 = $customRowThree['secondary_navigation_icon'];
+        $secondaryIconColor3 = $customRowThree['secondary_navigation_icon_color'];
         echo '<style>';
         echo '.menu-item {color: green}';
-        echo '.secondary-navigation li:nth-child(1) i { color: '.$secondaryNav1.'; }';
-        echo '.secondary-navigation li:nth-child(2) i { color: '.$secondaryNav2.'; }';
-        echo '.secondary-navigation li:nth-child(3) i { color: '.$secondaryNav3.'; }';
+        echo '.secondary-navigation li:nth-child(1) i { color: '.$secondaryIconColor1.'; }';
+        echo '.secondary-navigation li:nth-child(2) i { color: '.$secondaryIconColor2.'; }';
+        echo '.secondary-navigation li:nth-child(3) i { color: '.$secondaryIconColor3.'; }';
         echo '</style>';
         echo '<div class="secondary-menu-container">';
 
         $menuLocations = get_nav_menu_locations();
+
         if (has_nav_menu($secondaryMenu)){
             $menuID = $menuLocations[$secondaryMenu];
             $menuObject = wp_get_nav_menu_object($menuID);
             $menuSlug = $menuObject->slug;
             $menuItems = wp_get_nav_menu_items($menuID);
+            $itemCount = count($menuItems);
             echo '<ul id="menu-'.$menuSlug.'" class="menu secondary-navigation flex-wrap">';
+
             foreach($menuItems as $menuItem){
                 $menuItemTitle = $menuItem->title;
                 $menuItemUrl = $menuItem->url;
                 $menuItemID = $menuItem->ID;
-                $pos = strpos($menuItemTitle, 'Degree');
-                // if ($pos !== false){
-                //     echo "The string 'Degree' was found in the string '$menuItemTitle'";
-                //     echo "and its position is '$pos'";
-                //     $fontIcon = 'fa-poo';
-                //     echo $fontIcon;
-                // } else {
-                //     echo "The string 'Degree' was not found in the string '$menuItemTitle'";
-
-                //     $fontIcon = 'fa-thumbs-up';
-                //     echo $fontIcon;
-                // }
-
-                if (strpos($menuItemTitle, 'Degree') != false){
-                    $fontIcon = 'fa-poo';
-                    echo "woot!";
+                $pos = strpos($menuItemTitle, $secondaryNav1);
+                $pos1 = strpos($menuItemTitle, $secondaryNav2);
+                $pos2 = strpos($menuItemTitle, $secondaryNav3);
+                if ($pos !== false){
+                    $fontIcon = $secondaryIcon1;
+                } if ($pos1 !== false){
+                    $fontIcon = $secondaryIcon2;
+                } if ($pos2 !== false){
+                    $fontIcon = $secondaryIcon3;
                 }
-                if (strpos($menuItemTitle, 'Academic') != false){
-                    $fontIcon = 'fa-facebook';
-                    echo "woot!";
-                }
-                echo "<li id='menu-item-$menuItemID' class='menu-item menu-item-type-post_type menu-item-object-page menu-item-$menuItemID'><a href='$menuItemUrl'><i class='fas $fontIcon'></i><p class='chevron-right-yellow-small'>".$menuItemTitle."</p></a></li>";
+                echo "<li id='menu-item-$menuItemID' class='menu-item menu-item-type-post_type menu-item-object-page menu-item-$menuItemID'><a href='$menuItemUrl'><i class='fas fa-$fontIcon'></i><p class='chevron-right-yellow-small'>".$menuItemTitle."</p></a></li>";
             }
             echo '</ul>';
         }
         echo '</div>';
     }
-        var_dump($menuSlug);
-    if(is_page('academics')||is_page('research')){
-        if (has_nav_menu($secondaryMenu)){
-            wp_nav_menu( array(
-                'theme_location' => $secondaryMenu,
-                'link_before' => '<i class="fas '.$fontIcon.'"></i><p class="chevron-right-yellow-small">',
-                'link_after' => '</p>',
-                'menu_class' => 'menu secondary-navigation flex-wrap',
-            ) );
-        }
+// debug
+// echo '<pre>';
+// var_dump($secondaryNav3);
+// echo '</pre>';
+// end debug
+    // if(is_page('academics')||is_page('research')){
+    //     if (has_nav_menu($secondaryMenu)){
+    //         wp_nav_menu( array(
+    //             'theme_location' => $secondaryMenu,
+    //             'link_before' => '<i class="fas '.$fontIcon.'"></i><p class="chevron-right-yellow-small">',
+    //             'link_after' => '</p>',
+    //             'menu_class' => 'menu secondary-navigation flex-wrap',
+    //         ) );
+    //     }
 
-    }
+    // }
 ?>
     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
