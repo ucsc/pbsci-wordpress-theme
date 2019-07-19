1487cd03c813996b1a499774c4fe9e5594efc3b0
Jason Chafin <jchafin@ucsc.edu>
2019-07-19 16:50:08 -0700

    more work on panel cta

--- template-parts/footer-cta.php
+++ template-parts/footer-cta.php
@@ -12,7 +12,7 @@
     <div class="wrap">
         <div class="cta-container">
         <div><p class="cta-p1">do you</p><p class="cta-p2">Love science?</p></div>
-        <p class="cta-p3">Get our IMPACTREPORT for world-changing research, events, awards, unique opportunities, & more!</p><button type="button"><i class="fas fa-check"></i>SIGN ME UP!</button>
+        <p class="cta-p3">Get our IMPACTREPORT for world-changing research, events, awards, unique opportunities, & more!</p><a class="button" href="#"><i class="fas fa-check"></i>SIGN ME UP!</a>
         </div>
     </div>
 </aside>
