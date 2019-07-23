cddb897901d930a7500bde9a9b10fcef89b01091
Jason Chafin <jchafin@ucsc.edu>
2019-07-23 15:11:56 -0700

    working on footer CTA

--- template-parts/footer-cta.php
+++ template-parts/footer-cta.php
@@ -12,7 +12,7 @@
     <div class="wrap">
         <div class="cta-container">
         <div><p class="cta-p1">do you</p><p class="cta-p2">Love science?</p></div>
-        <p class="cta-p3">Get our IMPACTREPORT for world-changing research, events, awards, unique opportunities, & more!</p><a class="button" href="#"><i class="fas fa-check"></i>SIGN ME UP!</a>
+        <p class="cta-p3">Get our <span class="impact">IMPACT</span><span class="report">REPORT</span> for world-changing research, events, awards, unique opportunities, & more!</p><a class="button" href="#"><i class="fas fa-check"></i>SIGN ME UP!</a>
         </div>
     </div>
 </aside>
