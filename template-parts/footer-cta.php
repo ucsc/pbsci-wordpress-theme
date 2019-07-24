046b4b8b07e0833b4266f4a6986b44159469b4b6
Jason Chafin <jchafin@ucsc.edu>
2019-07-23 17:34:55 -0700

    fine-tuning css in cta and footer widget titles

--- template-parts/footer-cta.php
+++ template-parts/footer-cta.php
@@ -11,8 +11,8 @@
 <aside class="panel cta">
     <div class="wrap">
         <div class="cta-container">
-        <div><p class="cta-p1">do you</p><p class="cta-p2">Love science?</p></div>
-        <p class="cta-p3">Get our <span class="impact">IMPACT</span><span class="report">REPORT</span> for world-changing research, events, awards, unique opportunities, & more!</p><a class="button" href="#"><i class="fas fa-check"></i>SIGN ME UP!</a>
+        <div class="cta-headline"><p class="cta-p1">do you</p><p class="cta-p2">Love science?</p></div>
+        <p class="cta-p3">Get our <span class="impact">IMPACT</span><span class="report">REPORT</span> for world-changing research, events, awards, unique opportunities, & more!</p><a class="button" href="#"><i class="fas fa-check"></i>SIGN ME UP!</a><div class="clear"></div>
         </div>
     </div>
 </aside>
