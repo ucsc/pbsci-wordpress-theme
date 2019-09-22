6013c89e2d1dad70f990522461d6d6d861a28948
Jason Chafin <jchafin@ucsc.edu>
2019-09-22 10:40:23 -0700

    working on cat/tag archive

--- archive.php
+++ archive.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying archive pages
  *
@@ -7,46 +8,46 @@
  * @package UCSC_PBSci
  */
 
-get_header();
+get_header('archive');
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+<div id="primary" class="content-area">
+    <main id="main" class="site-main">
+        <div class="wrap">
+            <?php if (have_posts()) : ?>
 
-		<?php if ( have_posts() ) : ?>
+            <header class="page-header">
+                <?php
+						the_archive_title('<h1 class="page-title">', '</h1>');
+						the_archive_description('<div class="archive-description">', '</div>');
+						?>
+            </header><!-- .page-header -->
 
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+            <?php
+				/* Start the Loop */
+				while (have_posts()) :
+					the_post();
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
+					/*
 				 * Include the Post-Type-specific template for the content.
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+					get_template_part('template-parts/content', get_post_type());
 
-			endwhile;
+				endwhile;
 
-			the_posts_navigation();
+				the_posts_navigation();
 
-		else :
+			else :
 
-			get_template_part( 'template-parts/content', 'none' );
+				get_template_part('template-parts/content', 'none');
 
-		endif;
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+			endif;
+			?>
+        </div>
+    </main><!-- #main -->
+</div><!-- #primary -->
 
 <?php
 get_sidebar();
