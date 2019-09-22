d7c8400dfd89348605842d084c0ab2892f8ad9e3
Jason Chafin <jchafin@ucsc.edu>
2019-09-22 15:29:25 -0700

    first draft archive.php

--- template-parts/content-archive.php
+++ template-parts/content-archive.php
@@ -11,25 +11,16 @@
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <header class="entry-header">
-        <?php
-        if (is_singular()) :
-            if ('post' === get_post_type()) :
-                ?>
         <div class="entry-meta">
             <?php
-                            ucsc_pbsci_posted_on();
-                            ?>
+            ucsc_pbsci_posted_on();
+            ?>
         </div><!-- .entry-meta -->
-        <?php endif;
-            the_title('<h1 class="entry-title">', '</h1>');
-        else :
-            the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
-        endif;
-        ?>
 
     </header><!-- .entry-header -->
+    <?php ucsc_pbsci_post_thumbnail();
+    the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
+    ?>
 
-    <footer class="entry-footer">
-        <?php ucsc_pbsci_entry_footer(); ?>
-    </footer><!-- .entry-footer -->
+
 </article><!-- #post-<?php the_ID(); ?> -->
