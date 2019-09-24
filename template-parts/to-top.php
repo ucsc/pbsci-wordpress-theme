7fa9da299e63f05bb4bbbcebc26a5ae3dc60ddae
Jason Chafin <jchafin@ucsc.edu>
2019-09-24 07:40:29 -0700

    finished back to top

--- template-parts/to-top.php
+++ template-parts/to-top.php
@@ -4,6 +4,6 @@
  * Back to top
  */
 ?>
-<a class="topbutton" href="#" style="display: inline;">
+<a class="topbutton" href="#" style="display: none;">
     <i class="fa fa-chevron-up fa-2x"></i>
 </a>
