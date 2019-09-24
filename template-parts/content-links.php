20ba6e10d02b0bb9cf6fb3af163ae95b7576e066
Jason Chafin <jchafin@ucsc.edu>
2019-09-23 20:36:27 -0700

    first working draft of Support Opportunities CPT #28

--- template-parts/content-links.php
+++ template-parts/content-links.php
@@ -46,7 +46,7 @@
             $itemClass1 = 'student_support';
             $itemClass2 = 'opportunity_eligibility';
             $itemClass3 = 'opportunity_availability';
-        } elseif (is_page('Support')) {
+        } elseif (is_page('support')) {
             echo '<div id="page-' . $pslug . '" class="page-content">';
             get_template_part('template-parts/filter', 'support');
             $postType = 'support-science';
@@ -153,6 +153,25 @@
                         }
                         if ($postTax3) {
                             $taxTerms3 = get_the_terms($post->ID, $postTax3);
+                        }
+                    } elseif ($postType == 'support-science') {
+                        // get departments ACF
+                        $departments = get_field('department_link_global');
+                        //Post Type Taxonomies
+                        $postTax1 = 'support-science-cat';
+                        $postTax2 = 'support-science-int';
+                        // $postTax3 = 'student-opp-avail-tax';
+                        //Taxonomy Labels
+                        $taxLabel1 = 'Category';
+                        $taxLabel2 = 'Interest';
+
+                        //Terms conditionals
+                        if ($postTax1) {
+                            $taxTerms1 = get_the_terms($post->ID, $postTax1);
+                            $taxTerms1a = get_the_terms($post->ID, $postTax1);
+                        }
+                        if ($postTax2) {
+                            $taxTerms2 = get_the_terms($post->ID, $postTax2);
                         }
                     }
 
