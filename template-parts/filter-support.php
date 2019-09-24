f6b0ba7d85c01b082a41574ce38adacbcec85ac8
Jason Chafin <jchafin@ucsc.edu>
2019-09-24 06:15:47 -0700

    finished filter functionality for suppport cpt, #28. Closes:#28

--- template-parts/filter-support.php
+++ template-parts/filter-support.php
@@ -19,26 +19,21 @@
 // end debug
 
 // load taxonomy slugs into variables
-$postTax1 = 'student-support-tax';
-$postTax2 = 'student-opp-eligib-tax';
-$postTax3 = 'student-opp-avail-tax';
-// id
-$selectID = 'student_opportunities_list';
+$postTax1 = 'support-science-cat';
+$postTax2 = 'support-science-int';
+
+// id for listjs to hook into
+$selectID = 'support_science_list';
 // select value for null / top select drop-down values
-$selectTitle1 = 'Student Support Opportunities';
-$selectTitle2 = 'By eligibility';
-$selectTitle3 = 'By availability';
+$selectTitle1 = 'Support Categories';
+$selectTitle2 = 'By interest';
+
 // all taxonomy terms per taxonomy -- used to build select
 $taxTerms1 = get_terms($postTax1, ['hide_empty' => false]);
 $taxTerms2 = get_terms($postTax2, ['hide_empty' => false]);
-$taxTerms3 = get_terms($postTax3, ['hide_empty' => false]);
-
-
-
 ?>
 <h3 class="filter-head">Filter this list:</h3>
 <ul class="filter-list no-list-style">
-
     <li>
         <select class="filter-select" id="<?php echo $postTax1 ?>">
             <option selected="selected" value="clear"><?php echo $selectTitle1 ?></option>
@@ -50,7 +45,7 @@ $taxTerms3 = get_terms($postTax3, ['hide_empty' => false]);
         </select>
     </li>
     <li>
-        <select class="filter-select" id="student-supp-eligib-tax">
+        <select class="filter-select" id="<?php echo $postTax2 ?>">
             <option selected="selected" value="clear"><?php echo $selectTitle2 ?></option>
             <?php if ($taxTerms2) {
                 foreach ($taxTerms2 as $taxTerm2) {
@@ -60,17 +55,7 @@ $taxTerms3 = get_terms($postTax3, ['hide_empty' => false]);
         </select>
     </li>
     <li>
-        <select class="filter-select" id="student-supp-avail-tax">
-            <option selected="selected" value="clear"><?php echo $selectTitle3 ?></option>
-            <?php if ($taxTerms3) {
-                foreach ($taxTerms3 as $taxTerm3) {
-                    echo '<option value="' . $taxTerm3->slug . '">' . $taxTerm3->name . '</option>';
-                }
-            } ?>
-        </select>
-    </li>
-    <li>
-        <select class="filter-select" id="studentsupport-department-select">
+        <select class="filter-select" id="support-science-department-select">
             <option selected="selected" value="clear">By department</option>
             <?php
             $depargs = array(
@@ -93,10 +78,10 @@ $taxTerms3 = get_terms($postTax3, ['hide_empty' => false]);
         </select>
     </li>
     <li>
-        <input type="search" class="search" id="support-search" placeholder="Search by keyword" />
+        <input type="search" class="search" id="support-science-search" placeholder="Search by keyword" />
     </li>
     <li>
-        <input value="Clear all filters" type="reset" class="primary filter-clear" id="support-clear">
+        <input value="Clear all filters" type="reset" class="primary filter-clear" id="support-science-clear">
 
     </li>
 </ul>
