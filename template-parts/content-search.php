366611de90421f336dadddee324b50e4f59e186d
Jason Chafin <jchafin@ucsc.edu>
2019-10-24 06:56:22 -0700

    reworked permalinks on search results to include external links

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -15,27 +15,36 @@
         <header class="entry-header">
         <?php 
             if ('post' === get_post_type( get_the_ID() )):
-            $postType = 'a <span>News</span> post';
+                $postType = 'a <span>News</span> post';
+                $postUrl = get_permalink();
             elseif ('student-support' === get_post_type( get_the_ID() )):
                 $postType = 'an <span>Academic Support</span> post';
+                $postUrl = get_field('external_url');
             elseif ('labs' === get_post_type( get_the_ID() )):
                 $postType = 'a <span>Faculty & Researchers</span> post';
+                $postUrl = get_field('external_url');
             elseif ('support-science' === get_post_type( get_the_ID() )):
                 $postType = 'a <span>Support Funds</span> post';
+                $postUrl = get_field('external_url');
             elseif ('studentopportunities' === get_post_type( get_the_ID() )):
                 $postType = 'a <span>Research Opportunities</span> post';
+                $postUrl = get_field('external_url');
             elseif ('institutes-centers' === get_post_type( get_the_ID() )):
                 $postType = 'a <span>Groups & Facilities</span> post';
+                $postUrl = get_field('external_url');
             elseif ('degree' === get_post_type( get_the_ID() )):
                 $postType = 'a <span>Degree</span> post';
+                $postUrl = get_permalink();
             elseif ('department' === get_post_type( get_the_ID() )):
                 $postType = 'a <span>Department</span> post';
+                $postUrl = get_field('department_website');
             elseif ('page' === get_post_type( get_the_ID() )):
                 $postType = 'a <span>Page</span>';
+                $postUrl = get_permalink();
             endif;
             ?>
             <p class="archive-meta">This is <?php echo $postType ?></p>
-            <?php the_title(sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>'); ?>
+            <?php the_title(sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url($postUrl)), '</a></h2>'); ?>
 
             <?php if ('post' === get_post_type()) : ?>
             <div class="entry-meta">
