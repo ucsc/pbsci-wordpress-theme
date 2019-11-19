9346677f5c401e76fdd67805e7038c1735508f29
Jason Chafin <jchafin@ucsc.edu>
2019-11-18 16:03:06 -0800

    added eslint and dependencies to devDependencies in package.json, .eslintrc

--- js/back-to-top.js
+++ js/back-to-top.js
@@ -1,4 +1,3 @@
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -6,18 +5,18 @@
  * @link https://premium.wpmudev.org/blog/back-to-top-button-wordpress/
  */
 
-jQuery(document).ready(function ($) {
-    var offset = 100;
-    var speed = 250;
-    var duration = 500;
-    $(window).scroll(function () {
+jQuery(document).ready(function($) {
+    const offset = 100;
+    const speed = 250;
+    const duration = 500;
+    $(window).scroll(function() {
         if ($(this).scrollTop() < offset) {
             $('.topbutton').fadeOut(duration);
         } else {
             $('.topbutton').fadeIn(duration);
         }
     });
-    $('.topbutton').on('click', function () {
+    $('.topbutton').on('click', function() {
         $('html, body').animate({ scrollTop: 0 }, speed);
         return false;
     });
--- js/flex-home.js
+++ js/flex-home.js
@@ -1,35 +1,33 @@
-(function () {
+(function() {
+    // store the slider in a local variable
+    const $window = $(window);
+    const flexslider = { vars: {} };
 
-  // store the slider in a local variable
-  var $window = $(window),
-    flexslider = { vars: {} };
+    // tiny helper function to add breakpoints
+    function getGridSize() {
+        return window.innerWidth < 600 ? 1 : window.innerWidth < 900 ? 2 : 3;
+    }
 
-  // tiny helper function to add breakpoints
-  function getGridSize() {
-    return (window.innerWidth < 600) ? 1 :
-      (window.innerWidth < 900) ? 2 : 3;
-  }
-
-  $window.load(function () {
-    $('.flexslider').flexslider({
-      animation: "slide",
-      animationLoop: false,
-      itemWidth: 350,
-      itemMargin: 5,
-      controlNav: false,
-      minItems: getGridSize(), // use function to pull in initial value
-      maxItems: getGridSize(), // use function to pull in initial value
-      prevText: "",    //String: Set the text for the "previous" directionNav item
-      nextText: "",        //String: Set the text for the "next" directionNav item
-      directionNav: false, //
+    $window.load(function() {
+        $('.flexslider').flexslider({
+            animation: 'slide',
+            animationLoop: false,
+            itemWidth: 350,
+            itemMargin: 5,
+            controlNav: false,
+            minItems: getGridSize(), // use function to pull in initial value
+            maxItems: getGridSize(), // use function to pull in initial value
+            prevText: '', // String: Set the text for the "previous" directionNav item
+            nextText: '', // String: Set the text for the "next" directionNav item
+            directionNav: false, //
+        });
     });
-  });
 
-  // check grid size on resize event
-  $window.resize(function () {
-    var gridSize = getGridSize();
+    // check grid size on resize event
+    $window.resize(function() {
+        const gridSize = getGridSize();
 
-    flexslider.vars.minItems = gridSize;
-    flexslider.vars.maxItems = gridSize;
-  });
-}());
+        flexslider.vars.minItems = gridSize;
+        flexslider.vars.maxItems = gridSize;
+    });
+})();
